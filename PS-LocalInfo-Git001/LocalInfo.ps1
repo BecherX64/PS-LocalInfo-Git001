a6d8d7d739c7049e70cb8f6178120db5990eb1e2
BecherX64 <34269710+BecherX64@users.noreply.github.com>
2019-03-19 15:59:29 +0100

    Update #01
    Added $Output

--- PS-LocalInfo-Git001/LocalInfo.ps1
+++ PS-LocalInfo-Git001/LocalInfo.ps1
@@ -4,17 +4,23 @@
 
 #Branch 02 & Commit 03
 
+$Output = "C:\Temp\OutPut_" + $date + "_" + $Time + ".txt"
+If (Test-Path $Output) 
+{
+	Remove-Item $Output
+}
+
 $services = Get-Service
 $ComputerInfo = Get-WMIObject Win32_ComputerSystem
-Write-Host "Computer Name:" $ComputerInfo.Name -ForegroundColor Green
-Write-Host "----=====Computer Info====-----" -ForegroundColor Green
+Write-Host "Computer Name:" $ComputerInfo.Name -ForegroundColor Green | Add-Content $Output
+Write-Host "----=====Computer Info====-----" -ForegroundColor Green | Add-Content $Output
 $ComputerInfo.Properties | ForEach-Object `
 	{
-		$_.Name +";" + $_.Value
+		$_.Name +";" + $_.Value | Add-Content $Output
 	}
 
-Write-Host "----=====Running Services====-----" -ForegroundColor Green
+Write-Host "----=====Running Services====-----" -ForegroundColor Green | Add-Content $Output
 Foreach ($service in $services)
 {
-	Write-Host $service.Name ";" $service.Status ";" $service.StartType
+	Write-Host $service.Name ";" $service.Status ";" $service.StartType | Add-Content $Output
 }
