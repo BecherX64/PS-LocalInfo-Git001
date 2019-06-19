bd2f81832c0db2473b2ecc7a5cad1409287cab6c
Batis, Ivan <ivan.batis@gmail.com>
2019-06-19 16:27:32 +0200

    #Commit: 2019-06-19: TestSyntax.ps1 added

---
+++ PS-LocalInfo-Git001/TestSyntax.ps1
@@ -0,0 +1,30 @@
+#
+$file = ".\PS-LocalInfo-Git001\WebPage\Index.html"
+$pattern = "Version 1."
+$Error.Clear()
+
+#$CurentPath = pwd
+#Write-Host "Curent path:" $CurentPath
+#ls $file
+
+If (Test-Path $file)
+{
+	$content = Get-Content $file
+
+	$select = $content | Select-String -Pattern $pattern
+
+	if ($select) 
+	{
+		Write-Host "String found:" $select
+	} else 
+	{
+		throw "Required String not found"
+	}
+
+} else 
+{
+	Write-Host "something went wrong"
+	throw $Error
+}
+
+
